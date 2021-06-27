54da0e9e41bd2a99ac306beaa083b1ff2d139c47
stormzy22 <aawweb23@gmail.com>
2021-06-27 06:46:49 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useForm } from "./useForm";
 
 const App = () => {
+  const [value, handleChange] = useForm({ email: "", password: "" });
+
   return (
     <div>
-      <div>return app</div>
+      <input name="email" value={value.email} onChange={handleChange} />
+      <br />
+      <input
+        type="password"
+        name="password"
+        value={value.password}
+        onChange={handleChange}
+      />
+      <div>{value.email}</div>
+      <div>{value.password}</div>
     </div>
   );
 };
